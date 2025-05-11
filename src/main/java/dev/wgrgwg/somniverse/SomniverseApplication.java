d0ad1adca6a497986dfc87260cb8300c33b5d253
wgrgwg <kyoeng14697@naver.com>
2025-05-11 21:08:10 +0900

    initial commit

---
+++ src/main/java/dev/wgrgwg/somniverse/SomniverseApplication.java
@@ -0,0 +1,13 @@
+package dev.wgrgwg.somniverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomniverseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SomniverseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/wgrgwg/somniverse/SomniverseApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wgrgwg.somniverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SomniverseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
