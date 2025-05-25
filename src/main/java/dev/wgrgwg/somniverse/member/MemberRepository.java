02077acd9b462c40995acbf9dc4152301525f9ef
wgrgwg <kyoeng14697@naver.com>
2025-05-25 23:43:11 +0900

    feat(member): MemberRepository 내 쿼리 메서드 생성
    existsByEmail, existsByUsername 쿼리 메서드 생성

--- src/main/java/dev/wgrgwg/somniverse/member/MemberRepository.java
+++ src/main/java/dev/wgrgwg/somniverse/member/MemberRepository.java
@@ -1,9 +1,15 @@
 package dev.wgrgwg.somniverse.member;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Optional<Member> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+
+    Boolean existsByUsername(String username);
 }
