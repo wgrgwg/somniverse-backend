0d073391d7de36a74c3c1e4c00bc718f3f2015cb
wgrgwg <kyoeng14697@naver.com>
2025-06-08 20:30:10 +0900

    feat(security): CustomUserDetails 생성, 구현
    UserDetails interface를 구현하는 CustomUserDetails 생성, 구현

---
+++ src/main/java/dev/wgrgwg/somniverse/security/userdetails/CustomUserDetails.java
@@ -0,0 +1,52 @@
+package dev.wgrgwg.somniverse.security.userdetails;
+
+import dev.wgrgwg.somniverse.member.domain.Member;
+import java.util.Collection;
+import java.util.Collections;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final Member member;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(member.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return !member.isDeleted();
+    }
+}
