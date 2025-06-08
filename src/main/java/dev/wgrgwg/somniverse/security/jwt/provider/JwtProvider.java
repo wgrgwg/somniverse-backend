6a5c13145eeac96256c94e64ac5883ba1e47c482
wgrgwg <kyoeng14697@naver.com>
2025-06-09 00:05:16 +0900

    feat(jwt): validateToken, getAuthentication 구현
    토큰 유효성 검증, Authentication 객체 생성 로직 구현

--- src/main/java/dev/wgrgwg/somniverse/security/jwt/provider/JwtProvider.java
+++ src/main/java/dev/wgrgwg/somniverse/security/jwt/provider/JwtProvider.java
@@ -1,12 +1,21 @@
 package dev.wgrgwg.somniverse.security.jwt.provider;
 
 import dev.wgrgwg.somniverse.member.domain.Member;
+import dev.wgrgwg.somniverse.member.domain.Role;
 import dev.wgrgwg.somniverse.security.jwt.dto.TokenDto;
 import dev.wgrgwg.somniverse.security.jwt.properties.JwtProperties;
+import dev.wgrgwg.somniverse.security.userdetails.CustomUserDetails;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -40,5 +49,35 @@ public class JwtProvider {
                 new Date(System.currentTimeMillis() + expiredMs))
             .signWith(jwtProperties.getSecretKey())
             .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            return !getClaims(token).getExpiration().before(new Date());
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Authentication getAuthentication(String token) {
+        Claims claims = getClaims(token);
+
+        Collection<? extends GrantedAuthority> authorities =
+            Arrays.stream(claims.get("role").toString().split(","))
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+
+        Long memberId = Long.parseLong(claims.getSubject());
+        Role role = Role.valueOf(claims.get("role").toString());
+
+        Member member = Member.builder().id(memberId).role(role).build();
+        CustomUserDetails userDetails = new CustomUserDetails(member);
+
+        return new UsernamePasswordAuthenticationToken(userDetails, null, authorities);
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser().verifyWith(jwtProperties.getSecretKey()).build()
+            .parseSignedClaims(token).getPayload();
     }
 }
