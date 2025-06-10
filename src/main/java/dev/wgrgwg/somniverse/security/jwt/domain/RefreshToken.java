6f57d2eb4967d8a2cbd5a13ce2ee49b7d2a14310
wgrgwg <kyoeng14697@naver.com>
2025-06-10 15:23:09 +0900

    feat(jwt) RefreshToken 엔티티 생성

---
+++ src/main/java/dev/wgrgwg/somniverse/security/jwt/domain/RefreshToken.java
@@ -0,0 +1,37 @@
+package dev.wgrgwg.somniverse.security.jwt.domain;
+
+import dev.wgrgwg.somniverse.member.domain.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class RefreshToken {
+
+    @Id
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(nullable = false)
+    private String value;
+
+    public RefreshToken updateValue(String value) {
+        this.value = value;
+        return this;
+    }
+}
