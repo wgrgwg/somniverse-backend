ef7c24198b0f4cd5c0de015d9dabec6ac8c19e13
wgrgwg <kyoeng14697@naver.com>
2025-06-10 23:51:41 +0900

    refactor(jwt): RefreshTokenRepository 메서드명 수정

--- src/main/java/dev/wgrgwg/somniverse/security/jwt/repository/RefreshTokenRepository.java
+++ src/main/java/dev/wgrgwg/somniverse/security/jwt/repository/RefreshTokenRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
-    Optional<RefreshToken> findByToken(String token);
+    Optional<RefreshToken> findByValue(String value);
 
     List<RefreshToken> findAllByMemberId(Long memberId);
 }
