23461ef694da6c1f4ef37b08df35539bb84c6c08
wgrgwg <kyoeng14697@naver.com>
2025-06-10 15:29:49 +0900

    feat(jwt): RefreshTokenRepository 생성

---
+++ src/main/java/dev/wgrgwg/somniverse/security/jwt/repository/RefreshTokenRepository.java
@@ -0,0 +1,13 @@
+package dev.wgrgwg.somniverse.security.jwt.repository;
+
+import dev.wgrgwg.somniverse.security.jwt.domain.RefreshToken;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    List<RefreshToken> findAllByMemberId(Long memberId);
+}
