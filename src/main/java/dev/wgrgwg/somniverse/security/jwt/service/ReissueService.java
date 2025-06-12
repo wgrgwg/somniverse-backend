af0dcaef3a4f8cdd6710498ad84c0a510452a2e6
wgrgwg <kyoeng14697@naver.com>
2025-06-12 21:15:00 +0900

    feat(jwt): Token 재발급 기능인 ReissueService 생성

---
+++ src/main/java/dev/wgrgwg/somniverse/security/jwt/service/ReissueService.java
@@ -0,0 +1,42 @@
+package dev.wgrgwg.somniverse.security.jwt.service;
+
+import dev.wgrgwg.somniverse.global.exception.CustomException;
+import dev.wgrgwg.somniverse.member.domain.Member;
+import dev.wgrgwg.somniverse.security.exception.SecurityErrorCode;
+import dev.wgrgwg.somniverse.security.jwt.domain.RefreshToken;
+import dev.wgrgwg.somniverse.security.jwt.dto.ReissueResponse;
+import dev.wgrgwg.somniverse.security.jwt.dto.TokenDto;
+import dev.wgrgwg.somniverse.security.jwt.provider.JwtProvider;
+import dev.wgrgwg.somniverse.security.jwt.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ReissueService {
+
+    private final JwtProvider jwtProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Transactional
+    public ReissueResponse reissue(String refreshToken) {
+        if (!jwtProvider.validateToken(refreshToken)) {
+            throw new CustomException(SecurityErrorCode.INVALID_REFRESH_TOKEN);
+        }
+
+        RefreshToken refreshTokenFromDB = refreshTokenRepository.findByValue(refreshToken)
+            .orElseThrow(() -> new CustomException(SecurityErrorCode.REFRESH_TOKEN_NOT_FOUND));
+
+        Member member = refreshTokenFromDB.getMember();
+        if (member == null) {
+            throw new CustomException(SecurityErrorCode.MEMBER_NOT_FOUND);
+        }
+
+        TokenDto newTokens = jwtProvider.generateToken(member);
+
+        refreshTokenFromDB.updateValue(newTokens.refreshToken());
+
+        return new ReissueResponse(newTokens.accessToken(), newTokens.refreshToken());
+    }
+}
