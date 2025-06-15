77434fd7787fda4c4d186813debb2642bffae810
wgrgwg <kyoeng14697@naver.com>
2025-06-15 23:28:51 +0900

    fix(member): refreshToken id값 자동생성으로 변경

--- src/main/java/dev/wgrgwg/somniverse/member/domain/RefreshToken.java
+++ src/main/java/dev/wgrgwg/somniverse/member/domain/RefreshToken.java
@@ -3,6 +3,8 @@ package dev.wgrgwg.somniverse.member.domain;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -20,6 +22,7 @@ import lombok.NoArgsConstructor;
 public class RefreshToken {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
