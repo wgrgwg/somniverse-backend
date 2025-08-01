69cc7c0e1326642d0e3439d5d5fcaa693575e91b
wgrgwg <kyoeng14697@naver.com>
2025-08-01 23:56:10 +0900

    feat(comment): Comment 관련 ErrorCode 필드 추가
    - PARENT_COMMENT_DELETED : 삭제된 댓글에 대댓글 작성 시 오류
    - COMMENT_FORBIDDEN : 권한(인가) 오류

--- src/main/java/dev/wgrgwg/somniverse/comment/exception/CommentErrorCode.java
+++ src/main/java/dev/wgrgwg/somniverse/comment/exception/CommentErrorCode.java
@@ -8,8 +8,11 @@ import org.springframework.http.HttpStatus;
 public enum CommentErrorCode implements ErrorCode {
     PARENT_NOT_IN_SAME_DREAM("COMMENT_001", "부모 댓글과 같은 꿈 일기에 속해있지 않습니다.", HttpStatus.BAD_REQUEST),
     REPLY_TO_REPLY_NOT_ALLOWED("COMMENT_002", "대댓글에 대댓글을 작성할 수 없습니다.", HttpStatus.BAD_REQUEST),
+    PARENT_COMMENT_DELETED("COMMENT_003", "삭제된 댓글에 대댓글을 작성할 수 없습니다.", HttpStatus.BAD_REQUEST),
 
-    COMMENT_NOT_FOUND("COMMENT_003", "해당 댓글을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+    COMMENT_NOT_FOUND("COMMENT_003", "해당 댓글을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+
+    COMMENT_FORBIDDEN("COMMENT_004", "해당 댓글에 대한 권한이 없습니다.", HttpStatus.FORBIDDEN);
 
     private final String code;
     private final String message;
