6b9e00a4d6598b9f5805958e1b1525f4ff2436ed
wgrgwg <kyoeng14697@naver.com>
2025-08-09 20:44:01 +0900

    feat(comment): CommentController 관리자용 메서드에 대해 권한 검사 추가
    @PreAuthorize으로 메서드 실행 전에 권한 검사 추가

--- src/main/java/dev/wgrgwg/somniverse/comment/controller/CommentController.java
+++ src/main/java/dev/wgrgwg/somniverse/comment/controller/CommentController.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -86,7 +87,8 @@ public class CommentController {
 
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
-    
+
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'MANAGER')")
     @DeleteMapping("/comments/admin/{commentId}")
     public ResponseEntity<ApiResponseDto<Void>> deleteCommentByAdmin(@PathVariable Long commentId,
         @AuthenticationPrincipal CustomUserDetails userDetails) {
