090227a354c5c77c5b3f5410c3747e87238fe593
wgrgwg <kyoeng14697@naver.com>
2025-09-16 18:35:35 +0900

    test: ClientRegistrationRepository @MockitoBean으로 추가

--- src/test/java/dev/wgrgwg/somniverse/SomniverseApplicationTests.java
+++ src/test/java/dev/wgrgwg/somniverse/SomniverseApplicationTests.java
@@ -2,9 +2,16 @@ package dev.wgrgwg.somniverse;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SomniverseApplicationTests {
+
+    @MockitoBean
+    private ClientRegistrationRepository clientRegistrationRepository;
 
     @Test
     void contextLoads() {
